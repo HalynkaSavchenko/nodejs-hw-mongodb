d9a493ca1e5de02d5a95144df9bd730733961c00
Halyna Savchenko <galasavchenko1@gmail.com>
2024-06-15 21:10:25 +0200

    done

---
+++ src/db/models/contact.js
@@ -0,0 +1,40 @@
+import { model, Schema } from 'mongoose';
+
+const contactsSchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        phoneNumber: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            validate: {
+                validator: function(v) {
+                    return /\S+@\S+\.\S+/.test(v);
+                },
+                message: props => `${props.value} is not a valid email!`
+            },
+            required: false
+        },
+        isFavourite: {
+            type: Boolean,
+            default: false
+        },
+        contactType: {
+            type: String,
+            enum: ['work', 'home', 'personal'],
+            required: true,
+            default: 'personal'
+        }
+    }, 
+    {
+        timestamps: true,
+        versionKey: false
+    }
+);
+
+export const ContactsCollection = model('contacts', contactsSchema);
