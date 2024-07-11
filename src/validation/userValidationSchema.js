e58b0ec677c7a5fbd18b2a884adabc8ceac8273c
Halyna Savchenko <galasavchenko1@gmail.com>
2024-07-11 19:40:54 +0200

    passwordErrorMessage

--- src/validation/userValidationSchema.js
+++ src/validation/userValidationSchema.js
@@ -2,13 +2,16 @@ import Joi from 'joi';
 import { emailRegex } from '../constants/contacts-constants.js';
 import { passwordRegex } from '../constants/users-constants.js';
 
+const passwordErrorMessage = 'Пароль повинен містити принаймні одну малу літеру, одну велику літеру, одну цифру, один спеціальний символ і бути не менше 8 символів довжиною.';
 export const userSignupSchema = Joi.object({
     name: Joi.string().min(3).max(30).required(),
     email: Joi.string().pattern(emailRegex).required(),
-    password: Joi.string().pattern(passwordRegex).required(),
+    password: Joi.string().pattern(passwordRegex).required().messages({
+        'string.pattern.base': passwordErrorMessage}),
 });
 
 export const userSigninSchema = Joi.object({
     email: Joi.string().pattern(emailRegex).required(),
-    password: Joi.string().pattern(passwordRegex).required(),
+    password: Joi.string().pattern(passwordRegex).required().messages({
+        'string.pattern.base': passwordErrorMessage}),
 });
